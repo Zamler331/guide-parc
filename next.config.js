49c999c339a676978732a9d1e43ebf9aaced1467
Zamler331 <florian.pelech@gmail.Com>
2026-06-10 22:16:51 +0200

    V0.2.3

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
+  clientsClaim: true,
   disable: process.env.NODE_ENV === "development",
 
   fallbacks: {
@@ -10,12 +11,29 @@ const withPWA = require("next-pwa")({
 
   runtimeCaching: [
     {
-      urlPattern: /^https?.*/,
+      urlPattern: ({ request }) => request.mode === "navigate",
       handler: "NetworkFirst",
       options: {
-        cacheName: "offlineCache",
+        cacheName: "pages-cache",
+        networkTimeoutSeconds: 3,
+        expiration: {
+          maxEntries: 80,
+          maxAgeSeconds: 30 * 24 * 60 * 60,
+        },
+      },
+    },
+    {
+      urlPattern: ({ request }) =>
+        request.destination === "image" ||
+        request.destination === "script" ||
+        request.destination === "style" ||
+        request.destination === "font",
+      handler: "CacheFirst",
+      options: {
+        cacheName: "static-assets-cache",
         expiration: {
           maxEntries: 200,
+          maxAgeSeconds: 30 * 24 * 60 * 60,
         },
       },
     },
